3b316a16fed4c309cd3be03c021e04665afa06fb
Your Name <jnlbeats5@gmail.com>
2025-08-17 09:30:12 +0300

    The admin model has been created

---
+++ models/admin.js
@@ -0,0 +1,18 @@
+const {Schema , model} = require("mongoose");
+
+
+const AdmingSchema = Schema({
+
+    Name: {
+        required: true,
+        type:String
+    },
+    email: {
+        required: true,
+        type:String
+    },
+    password:{
+        required: true,
+        type:String
+    }
+})

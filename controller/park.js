606e934087adbd52a9236b1bc9f157b53efa72df
Your Name <jnlbeats5@gmail.com>
2025-08-17 13:01:42 +0300

    Added more CRUD fuctionality

--- controller/park.js
+++ controller/park.js
@@ -24,3 +24,20 @@ async function getPark(request, response) {
     };
     
 };
+async function updatePark(request, response) {
+    
+    try {
+
+        const parkU = await park.findByIdAndUpdate(request.params.id , request.body);
+        if(parkU) {
+            response.status(200).json(park);
+         }
+            else {
+                response.sendStatus(404).json;
+            }
+
+    }catch(err) {
+        console.log("Their is an error updating Admin: ",err)
+    }
+}
+

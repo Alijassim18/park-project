cb78490a77789f11196e2a145a9e295fb481b45b
Your Name <jnlbeats5@gmail.com>
2025-08-17 12:58:59 +0300

    adding more CRUD functionalities in the park model

--- controller/park.js
+++ controller/park.js
@@ -1,3 +1,26 @@
 const park = require("../models/park");
 
 
+async function createPark(request, response) {
+
+    try {
+        console.log("In Route")
+
+        const parkk  = await park.create(request.body);
+                        response.status(201).json(parkk);
+
+    }catch(err) {
+        console.log("Their is an error creating Admin: ",err);
+        response.status(500).json({error : err.message});
+    }
+
+};
+async function getPark(request, response) {
+    try {
+        const parkGet  = await park.find();
+        response.status(201).json(parkGet);
+    }catch(err) {
+        console.log("Their is an error getting Admin: ",err);
+    };
+    
+};
